4c015864c222c231fe25828e387e9a88727b7341
jjmin321 <jjmin321@naver.com>
2020-10-14 15:55:28 +0900

    [UPDATE] App.Vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
+  @import "@/assets/styles/font.scss";
 
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: NotoSansKR;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
 </style>
