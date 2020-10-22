fa59b0f9b8a00566fb9520ec65993e176a28e70e
jjmin321 <jjmin321@naver.com>
2020-10-22 18:00:46 +0900

    [MODIFY] store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore } from 'vuex'
+// import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-    userName: "",
-    isLogin: false
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+// export default createStore({
+//   state: {
+//     userName: "",
+//     isLogin: false,
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
