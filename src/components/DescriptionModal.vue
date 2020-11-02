99212e256564f2db7741fc6c9b8e79d829486261
jjmin321 <jjmin321@naver.com>
2020-11-02 22:33:41 +0900

    [UPDATE] 안내사항 설명

--- src/components/DescriptionModal.vue
+++ src/components/DescriptionModal.vue
@@ -2,13 +2,15 @@
   <div class="description-modal">
         <div class="description-modal-area" @click="onClose"/>
         <div class="description-modal-box">
-            <h3 class="description-modal-box-title">대소고 랜덤채팅 웹</h3>
+            <h3 class="description-modal-box-title">대소고 랜덤채팅</h3>
             <ol>
-                <li class="description-modal-box-description">대구소프트웨어고등학교 2학년 2학기 프로젝트 실무 시간에 개발하였습니다</li>
-                <li class="description-modal-box-description">다른 사람이 기분이 나쁠만한 닉네임이나 채팅을 자제해주세요</li>
-                <li class="description-modal-box-description">자세한 정보와 코드는 <a href="https://github.com/jjmin321/randomchatting_web" target="_blank">이곳</a>에 있습니다</li>
+                <li class="description-modal-box-description">대구소프트웨어고등학교 학생들이 함께 사용할 수 있는 채팅 서비스입니다</li>
+                <li class="description-modal-box-description">익명성을 악용하여 타인에게 상처를 입히는 채팅은 자제해주시길 바랍니다</li>
+                <li class="description-modal-box-description">전체 채팅은 모두가 함께 사용하는 채팅방이며, 랜덤 채팅은 사람이 랜덤으로 매칭됩니다</li>
+                <li class="description-modal-box-description">전체 채팅과 랜덤 채팅이 동시에 접속되며, 클릭을 통해 두 채팅방을 이동할 수 있습니다</li>
+                <li class="description-modal-box-description">현재 접속되어 있는 유저 목록을 확인할 수 있고, 한 계정으로 다중 접속을 막아두었습니다</li>
+                <li class="description-modal-box-description">자세한 정보와 코드는 깃허브를 참고해 주세요 <a href="https://github.com/jjmin321/randomchatting_web" target="_blank">웹</a>, <a href="https://github.com/jjmin321/randomchatting_server" target="_blank">서버</a></li>
                 <li class="description-modal-box-description">버그 발견 및 질문, 요청사항은 <a href="https://www.facebook.com/profile.php?id=100028649371922" target="_blank">페메</a>로 알려주세요</li>
-                <li class="description-modal-box-description">(현재 버그)윈도우 환경에서 자동으로 스크롤이 안 내려가는 부분과 전체 채팅에서 사람 목록이 가끔 이상해지는 버그를 수정 중입니다</li>
             </ol>
         </div>
   </div>
